cd973cf9bb6d3dcc20c2520f8aeb9874876dd82f
Curthaldor <haldorsc@oregonstate.edu>
2021-01-21 10:23:25 -0800

    Implemented basic features
    Object creation has been removed, however this script will still detect and provide information on whatever object is clicked. The Clickmanager object merely keeps the script running.

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour{
+	// Start is called before the first frame update
+	void Update(){
+		if (Input.GetMouseButtonDown(0)){
+
+			Vector3 clickPosition = -Vector3.one;
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit)){
+				clickPosition = hit.point;
+			}
+
+			Debug.Log(clickPosition);
+		}
+	}
+}
