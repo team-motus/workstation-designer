4fa064c9863c75a35913cc83c3499cc28cc28d5a
Logan Traffas <logan.traffas@gmail.com>
2021-02-10 18:42:47 -0800

    Add camera controls to move main camera with user input
    Controls are WASD, Space/LeftShift, and panning with cursor

---
+++ Assets/Scripts/Util/MathUtil.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace WorkstationDesigner.Util
+{
+    public static class MathUtil
+    {
+        /// <summary>
+        /// Return true if both numbers have the same sign
+        /// </summary>
+        public static bool SameSign(double a, double b)
+        {
+            return (a < 0) == (b < 0);
+        }
+    }
+}
