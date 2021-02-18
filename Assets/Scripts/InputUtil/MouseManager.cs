a9ce83f6f04e3095b0262b2e4a22e9509a57dd28
Matthew Macovsky <macovsky.m@gmail.com>
2021-02-17 17:07:31 -0800

    Prevent placing components when the mouse is over the UI

--- Assets/Scripts/InputUtil/MouseManager.cs
+++ Assets/Scripts/InputUtil/MouseManager.cs
@@ -6,17 +6,19 @@ namespace WorkstationDesigner.InputUtil
     /// <summary>
     /// This class is used to get mouse button states that are blocked by the UI
     /// </summary>
-    public class MouseButtonManager: MonoBehaviour
+    public class MouseManager : MonoBehaviour
     {
         private const int NUM_MOUSE_BUTTONS = 3;
         private static readonly bool[] mouseButtonStates = new bool[NUM_MOUSE_BUTTONS];
+        private static bool mouseOver;
 
-        static MouseButtonManager()
+        static MouseManager()
         {
-            for(var i = 0; i < NUM_MOUSE_BUTTONS; i++)
+            for (var i = 0; i < NUM_MOUSE_BUTTONS; i++)
             {
                 mouseButtonStates[i] = false;
             }
+            mouseOver = false;
         }
 
         public void Update()
@@ -61,6 +63,24 @@ namespace WorkstationDesigner.InputUtil
             {
                 return mouseButtonStates[mouseButton];
             }
+        }
+
+        /// <summary>
+        /// Never call this function. Only UIBackdrop should call this function in its event callbacks.
+        /// </summary>
+        /// <param name="over">Whether the mouse is over the non-UI region or not</param>
+        public static void SetMouseOver(bool over)
+        {
+            mouseOver = over;
+        }
+
+        /// <summary>
+        /// Get the pressed state of a mouse button, as you would with Unity's Input.GetMouseButton
+        /// </summary>
+        /// <returns>Whether the mouse is over the non-UI region</returns>
+        public static bool GetMouseOver()
+        {
+            return mouseOver;
         }
     }
 }
