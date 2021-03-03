866c441d73cf3cff19ea130bee5e8bc7b0c69c51
trumanhess27 <hesstr@oregonstate.edu>
2021-03-02 16:24:16 -0800

    Added method of getting in and out of XR with key presses. Bug fixes. Deleted unused ExitXR script.

--- Assets/Scripts/XRUI.cs
+++ Assets/Scripts/XRUI.cs
@@ -5,15 +5,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 namespace WorkstationDesigner
 {
+    /// <summary>
+    /// Sets the behavior for the XR UI.
+    /// </summary>
     public class XRUI : MonoBehaviour
     {
         private GameObject canvas;
-        private GameObject rController;
-        private Camera xrCam;
+        private GameObject rController = null;
+        private Camera xrCam = null;
         private CameraSwitcher status;
-
-        //private Vector3 yRot;
-        //private Vector3 camDistance;
 
         private ActionBasedController c;
 
@@ -24,8 +24,6 @@ namespace WorkstationDesigner
 
         void Start()
         {
-            //xrCam = GameObject.FindWithTag("MainCamera");
-            xrCam = Camera.main;
             canvas = GameObject.Find("Canvas");
             canvas.SetActive(false);
 
@@ -33,17 +31,20 @@ namespace WorkstationDesigner
             status = temp.GetComponent<CameraSwitcher>();
         }
 
-        // Update is called once per frame
         void Update()
         {
             if(status.getXRRigStatus() == true)
-            {
+            {   
                 // Assign controller if it is null
                 if(rController == null)
                 {
                     rController = GameObject.Find("RightHand Controller");
                     c = rController.GetComponent<ActionBasedController>();
                 }
+
+                // XR camear must have MainCamera tag applied to it
+                if(xrCam == null)
+                    xrCam = Camera.main;
 
                 // Check for correct button press on the right hand controller
                 // Update UI and show it
@@ -62,17 +63,26 @@ namespace WorkstationDesigner
             }
         }
 
+        /// <summary>
+        /// Updates Y rotation and X and Z position of the XR UI.
+        /// </summary>
         void updateXRUI()
         {
             canvas.transform.position = targetDistance();
             canvas.transform.eulerAngles = targetRotation();
         }
 
+        /// <summary>
+        /// Returns a Vector3 of the main cameras Y angle with X and Z set to 0.
+        /// </summary>
         Vector3 targetRotation()
         {
             return new Vector3(0, xrCam.transform.eulerAngles.y, 0);
         }
 
+        /// <summary>
+        /// Returns a Vector3 based on the main cameras view position with a fixed height and distance from the camera.
+        /// </summary>
         Vector3 targetDistance()
         {
             Vector3 target;
